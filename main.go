62d291b68ff60a186bfe6b19ca0957192381b2c0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-02-04 18:31:14 +0100

    Handle outside lambda too

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type MyEvent struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Name string `json:"name"` // Name
+	Age  int    `json:"age"`  // Age
 }
 
 type MyResponse struct {
@@ -23,5 +25,29 @@ func HandleLambdaEvent(event *MyEvent) (*MyResponse, error) {
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	if _, exist := os.LookupEnv("AWS_LAMBDA_RUNTIME_API"); exist {
+		lambda.Start(HandleLambdaEvent)
+	} else {
+		event := MyEvent{}
+
+		flag.StringVar(&event.Name, "name", "", "Specify the name")
+		flag.IntVar(&event.Age, "age", 0, "Specify the age")
+
+		flag.Parse()
+
+		if event.Name == "" && event.Age == 0 {
+			fmt.Printf("Usage: %s\n\n", os.Args[0])
+			flag.PrintDefaults()
+			return
+		}
+
+		response, err := HandleLambdaEvent(&event)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, response)
+			os.Exit(1)
+		}
+
+		fmt.Println(response.Message)
+	}
 }
