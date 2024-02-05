e714b28a2a8b869fb35e1cc530f4430c6ffc94b3
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-02-05 11:55:45 +0100

    Register IP targets based on DNS name

--- go.mod
+++ go.mod
@@ -2,10 +2,15 @@ module github.com/dex4er/lambda-lb-target-group-dns
 
 go 1.21
 
-require github.com/aws/aws-lambda-go v1.46.0
+require (
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/aws/aws-sdk-go v1.50.10
+	github.com/hashicorp/logutils v1.0.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,53 +1,234 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/hashicorp/logutils"
 )
 
 type MyEvent struct {
-	Name string `json:"name"` // Name
-	Age  int    `json:"age"`  // Age
+	TargetGroupArn string `json:"targetGroupArn"` // Target Group ARN
+	DomainName     string `json:"domainName"`     // Domain name
 }
 
 type MyResponse struct {
-	Message string `json:"answer"`
+	Message string `json:"message"`
+}
+
+func getTargetGroup(svc *elbv2.ELBV2, targetGroupARN string) (*elbv2.TargetGroup, error) {
+	input := &elbv2.DescribeTargetGroupsInput{
+		TargetGroupArns: []*string{
+			aws.String(targetGroupARN),
+		},
+	}
+
+	output, err := svc.DescribeTargetGroups(input)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(output.TargetGroups) == 0 {
+		return nil, fmt.Errorf("target group %s not found", targetGroupARN)
+	}
+
+	return output.TargetGroups[0], nil
+}
+
+func getTargetGroupIPs(svc *elbv2.ELBV2, targetGroupArn string) ([]string, error) {
+	input := &elbv2.DescribeTargetHealthInput{
+		TargetGroupArn: aws.String(targetGroupArn),
+	}
+
+	result, err := svc.DescribeTargetHealth(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var ipAddresses []string
+	for _, targetHealth := range result.TargetHealthDescriptions {
+		if targetHealth.Target != nil && targetHealth.Target.Id != nil {
+			ipAddresses = append(ipAddresses, *targetHealth.Target.Id)
+		}
+	}
+
+	return ipAddresses, nil
+}
+
+func registerTarget(svc *elbv2.ELBV2, targetGroupArn string, targetIP string) error {
+	input := &elbv2.RegisterTargetsInput{
+		TargetGroupArn: aws.String(targetGroupArn),
+		Targets: []*elbv2.TargetDescription{
+			{
+				Id: aws.String(targetIP),
+			},
+		},
+	}
+
+	_, err := svc.RegisterTargets(input)
+	return err
+}
+
+func deregisterTarget(svc *elbv2.ELBV2, targetGroupArn string, targetIP string) error {
+	input := &elbv2.DeregisterTargetsInput{
+		TargetGroupArn: aws.String(targetGroupArn),
+		Targets: []*elbv2.TargetDescription{
+			{
+				Id: aws.String(targetIP),
+			},
+		},
+	}
+
+	_, err := svc.DeregisterTargets(input)
+	return err
+}
+
+func lookupIp(domain string, ipAddressType string) ([]string, error) {
+	ips, err := net.LookupIP(domain)
+	if err != nil {
+		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.Err == "no such host" {
+			// The domain does not exist: just return empty list
+			return []string{}, nil
+		}
+		return []string{}, err
+	}
+
+	var ipAddresses []string
+	for _, ip := range ips {
+		if ipAddressType == "ipv4" && ip.To4() != nil || ipAddressType == "ipv6" && ip.To4() == nil {
+			log.Printf("[TRACE] %s %s", ipAddressType, ip.String())
+			ipAddresses = append(ipAddresses, ip.String())
+		}
+	}
+
+	return ipAddresses, nil
 }
 
 func HandleLambdaEvent(event *MyEvent) (*MyResponse, error) {
+	var err error
+
 	if event == nil {
-		return nil, fmt.Errorf("received nil event")
+		return nil, errors.New("received nil event")
 	}
-	return &MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+
+	awsRegion := os.Getenv("AWS_REGION")
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Config: aws.Config{
+			Region:                        aws.String(awsRegion),
+			CredentialsChainVerboseErrors: aws.Bool(true),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	svc := elbv2.New(sess)
+
+	tg, err := getTargetGroup(svc, event.TargetGroupArn)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("[TRACE] TargetGroup %s", strings.ReplaceAll(strings.ReplaceAll(tg.String(), "\n  ", " "), "\n", " "))
+
+	if *tg.TargetType != "ip" {
+		return nil, errors.New("target group type must be ip")
+	}
+
+	dnsIpAddresses, err := lookupIp(event.DomainName, *tg.IpAddressType)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("[DEBUG] %s resolves to %v", event.DomainName, dnsIpAddresses)
+
+	dnsIpAddressesMap := make(map[string]bool)
+	for _, ip := range dnsIpAddresses {
+		dnsIpAddressesMap[ip] = true
+	}
+
+	tgIpAddresses, err := getTargetGroupIPs(svc, event.TargetGroupArn)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("[DEBUG] %s has targets %v", *tg.TargetGroupName, tgIpAddresses)
+
+	tgIpAddressesMap := make(map[string]bool)
+	for _, ip := range tgIpAddresses {
+		tgIpAddressesMap[ip] = true
+	}
+
+	for _, ip := range dnsIpAddresses {
+		if _, exists := tgIpAddressesMap[ip]; !exists {
+			log.Printf("[DEBUG] Adding %s to Target Group %s", ip, *tg.TargetGroupName)
+			err := registerTarget(svc, event.TargetGroupArn, ip)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	for _, ip := range tgIpAddresses {
+		if _, exists := dnsIpAddressesMap[ip]; !exists {
+			log.Printf("[DEBUG] Removing %s from Target Group %s", ip, *tg.TargetGroupName)
+			err := deregisterTarget(svc, event.TargetGroupArn, ip)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	tgIpAddresses2, err := getTargetGroupIPs(svc, event.TargetGroupArn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MyResponse{Message: fmt.Sprintf("Registered IP addresses of domain %s to Target Group %s: %v", event.DomainName, *tg.TargetGroupName, tgIpAddresses2)}, nil
 }
 
 func main() {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "TRACE"
+	}
+
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"TRACE", "DEBUG", "ERROR"},
+		MinLevel: logutils.LogLevel(logLevel),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+
 	if _, exist := os.LookupEnv("AWS_LAMBDA_RUNTIME_API"); exist {
 		lambda.Start(HandleLambdaEvent)
 	} else {
 		event := MyEvent{}
 
-		flag.StringVar(&event.Name, "name", "", "Specify the name")
-		flag.IntVar(&event.Age, "age", 0, "Specify the age")
+		flag.StringVar(&event.TargetGroupArn, "target-group-arn", "", "Target Group ARN")
+		flag.StringVar(&event.DomainName, "domain-name", "", "Domain name")
 
 		flag.Parse()
 
-		if event.Name == "" && event.Age == 0 {
+		if event.TargetGroupArn == "" || event.DomainName == "" {
 			fmt.Printf("Usage: %s\n\n", os.Args[0])
 			flag.PrintDefaults()
-			return
+			os.Exit(2)
 		}
 
 		response, err := HandleLambdaEvent(&event)
 
 		if err != nil {
-			fmt.Fprintln(os.Stderr, response)
-			os.Exit(1)
+			log.Fatalf("[ERROR] %v", err)
 		}
 
-		fmt.Println(response.Message)
+		log.Printf("[DEBUG] %v", response.Message)
 	}
 }
